69ae0f3aa9adb4a09197c27884c08153a335b876
pedrodecf <contato.pedrodecf@gmail.com>
2023-12-13 16:58:51 -0300

    chore: cors origin

--- src/server.js
+++ src/server.js
@@ -22,7 +22,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://127.0.0.1:5173/", "https://foodexplorer-pedrodecf.netlify.app/"],
+    origin: ["http://localhost:5173", "http://127.0.0.1:5173/", "https://foodexplorer-pedrodecf.netlify.app"],
     credentials: true,
   })
 )
